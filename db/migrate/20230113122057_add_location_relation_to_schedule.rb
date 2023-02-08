c212b70da0ec85e9544d4ae36124fa3f39dac855
Maxat Sangerbayev <m.sangerbayev@datarockets.com>
2023-02-08 19:28:56 +0600

    Revert "WIP on make schedule.location a belongs_to association"
    This reverts commit e6878688f6d00810072302ae5daa70b11da63705.

--- db/migrate/20230113122057_add_location_relation_to_schedule.rb
+++
@@ -1,6 +0,0 @@
-class AddLocationRelationToSchedule < ActiveRecord::Migration[5.2]
-  def change
-    rename_column :schedules, :location, :location_name
-    add_reference :schedules, :location, index: true, foreign_key: true
-  end
-end
