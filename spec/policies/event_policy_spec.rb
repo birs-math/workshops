d8749d1217c6f7fccea856c3e8f24293ab131d61
Mikhail Matyukhin <itsmatyukhin@gmail.com>
2024-04-12 14:03:02 +0100

    test: add new state related policies tests

--- spec/policies/event_policy_spec.rb
+++ spec/policies/event_policy_spec.rb
@@ -6,7 +6,7 @@
 
 require 'rails_helper'
 
-describe "EventPolicy" do
+describe 'EventPolicy' do
   before do
     @location = Setting.Locations.keys.first
   end
@@ -18,73 +18,155 @@ describe "EventPolicy" do
   let (:admin_user) { build_stubbed :user, :admin }
 
   permissions :edit? do
-    it 'does not allow normal users to edit events' do
-      allow(normal_user).to receive(:is_organizer?).and_return(false)
-      expect(subject).not_to permit(normal_user, Event.new)
+    (Event.states.keys - ['active']).each do |state|
+      context "when event has #{state} state" do
+        it 'does not allow normal users to edit events' do
+          allow(normal_user).to receive(:is_organizer?).and_return(false)
+          expect(subject).not_to permit(normal_user, Event.new(state: state))
+        end
+
+        it 'does not allow staff to edit events in different location' do
+          staff_user.location = 'foo'
+          event = create(:event, location: 'bar', state: state)
+          expect(subject).not_to permit(staff_user, event)
+        end
+
+        it 'allows staff to edit events in same location' do
+          staff_user.location = 'foo'
+          event = create(:event, location: 'foo', state: state)
+          expect(subject).to permit(staff_user, event)
+        end
+
+        it 'allows staff to edit template events' do
+          allow(staff_user).to receive(:is_organizer?).and_return(false)
+          event = Event.new(template: true, location: @location, state: state)
+          expect(subject).to permit(staff_user, event)
+        end
+
+        it 'allows organizers to edit their events' do
+          allow(normal_user).to receive(:is_organizer?).and_return(true)
+          expect(subject).not_to permit(normal_user, Event.new(state: state))
+        end
+
+        it 'allows admins to edit events' do
+          allow(admin_user).to receive(:is_organizer?).and_return(false)
+          expect(subject).to permit(admin_user, Event.new(state: state))
+        end
+      end
+
+      context 'when event has active state' do
+        it 'does not allow normal users to edit events' do
+          allow(normal_user).to receive(:is_organizer?).and_return(false)
+          expect(subject).not_to permit(normal_user, Event.new(state: state))
+        end
+
+        it 'does not allow staff to edit events in different location' do
+          staff_user.location = 'foo'
+          event = create(:event, location: 'bar', state: :active)
+          expect(subject).not_to permit(staff_user, event)
+        end
+
+        it 'allows staff to edit events in same location' do
+          staff_user.location = 'foo'
+          event = create(:event, location: 'foo', state: :active)
+          expect(subject).to permit(staff_user, event)
+        end
+
+        it 'allows staff to edit template events' do
+          allow(staff_user).to receive(:is_organizer?).and_return(false)
+          event = Event.new(template: true, location: @location, state: :active)
+          expect(subject).to permit(staff_user, event)
+        end
+
+        it 'allows organizers to edit their events' do
+          allow(normal_user).to receive(:is_organizer?).and_return(true)
+          expect(subject).to permit(normal_user, Event.new(state: :active))
+        end
+
+        it 'allows admins to edit events' do
+          allow(admin_user).to receive(:is_organizer?).and_return(false)
+          expect(subject).to permit(admin_user, Event.new(state: :active))
+        end
+      end
     end
 
-    it 'does not allow staff to edit events in different location' do
-      staff_user.location = 'foo'
-      event = create(:event, location: 'bar')
-      expect(subject).not_to permit(staff_user, event)
-    end
-
-    it 'allows staff to edit events in same location' do
-      staff_user.location = 'foo'
-      event = create(:event, location: 'foo')
-      expect(subject).to permit(staff_user, event)
-    end
-
-    it 'allows staff to edit template events' do
-      allow(staff_user).to receive(:is_organizer?).and_return(false)
-      event = Event.new(template: true, location: @location)
-      expect(subject).to permit(staff_user, event)
-    end
-
-    it 'allows organizers to edit their events' do
-      allow(normal_user).to receive(:is_organizer?).and_return(true)
-      expect(subject).to permit(normal_user, Event.new)
-    end
-
-    it 'allows admins to edit events' do
-      allow(admin_user).to receive(:is_organizer?).and_return(false)
-      expect(subject).to permit(admin_user, Event.new)
-    end
   end
 
   permissions :update? do
-    it 'does not allow normal users to update events' do
-      allow(normal_user).to receive(:is_organizer?).and_return(false)
-      expect(subject).not_to permit(normal_user, Event.new)
+    (Event.states.keys - ['active']).each do |state|
+      context "when event has #{state} state" do
+        it 'does not allow normal users to update events' do
+          allow(normal_user).to receive(:is_organizer?).and_return(false)
+          expect(subject).not_to permit(normal_user, Event.new(state: state))
+        end
+
+        it 'does not allow staff users to update events in different location' do
+          staff_user.location = 'foo'
+          event = create(:event, location: 'bar', state: state)
+          allow(staff_user).to receive(:is_organizer?).and_return(false)
+          expect(subject).not_to permit(staff_user, event)
+        end
+
+        it 'allows staff to edit events in same location' do
+          staff_user.location = 'foo'
+          event = create(:event, location: 'foo', state: state)
+          expect(subject).to permit(staff_user, event)
+        end
+
+        it 'allows staff to update template events' do
+          allow(staff_user).to receive(:is_organizer?).and_return(false)
+          expect(subject).to permit(staff_user, Event.new(template: true,
+                                                          location: @location,
+                                                          state: state))
+        end
+
+        it 'allows organizers to edit their events' do
+          allow(normal_user).to receive(:is_organizer?).and_return(true)
+          expect(subject).not_to permit(normal_user, Event.new(state: state))
+        end
+
+        it 'allows admins to update events' do
+          allow(admin_user).to receive(:is_organizer?).and_return(false)
+          expect(subject).to permit(admin_user, Event.new(state: state))
+        end
+      end
     end
 
-    it 'does not allow staff users to update events in different location' do
-      staff_user.location = 'foo'
-      event = create(:event, location: 'bar')
-      allow(staff_user).to receive(:is_organizer?).and_return(false)
-      expect(subject).not_to permit(staff_user, event)
-    end
+    context 'when event has active state' do
+      it 'does not allow normal users to update events' do
+        allow(normal_user).to receive(:is_organizer?).and_return(false)
+        expect(subject).not_to permit(normal_user, Event.new(state: :active))
+      end
 
-    it 'allows staff to edit events in same location' do
-      staff_user.location = 'foo'
-      event = create(:event, location: 'foo')
-      expect(subject).to permit(staff_user, event)
-    end
+      it 'does not allow staff users to update events in different location' do
+        staff_user.location = 'foo'
+        event = create(:event, location: 'bar')
+        allow(staff_user).to receive(:is_organizer?).and_return(false)
+        expect(subject).not_to permit(staff_user, event)
+      end
 
-    it 'allows staff to update template events' do
-      allow(staff_user).to receive(:is_organizer?).and_return(false)
-      expect(subject).to permit(staff_user, Event.new(template: true,
-        location: @location))
-    end
+      it 'allows staff to edit events in same location' do
+        staff_user.location = 'foo'
+        event = create(:event, location: 'foo')
+        expect(subject).to permit(staff_user, event)
+      end
 
-    it 'allows organizers to edit their events' do
-      allow(normal_user).to receive(:is_organizer?).and_return(true)
-      expect(subject).to permit(normal_user, Event.new)
-    end
+      it 'allows staff to update template events' do
+        allow(staff_user).to receive(:is_organizer?).and_return(false)
+        expect(subject).to permit(staff_user, Event.new(template: true,
+                                                        location: @location,
+                                                        state: :active))
+      end
 
-    it 'allows admins to update events' do
-      allow(admin_user).to receive(:is_organizer?).and_return(false)
-      expect(subject).to permit(admin_user, Event.new)
+      it 'allows organizers to edit their events' do
+        allow(normal_user).to receive(:is_organizer?).and_return(true)
+        expect(subject).to permit(normal_user, Event.new(state: :active))
+      end
+
+      it 'allows admins to update events' do
+        allow(admin_user).to receive(:is_organizer?).and_return(false)
+        expect(subject).to permit(admin_user, Event.new(state: :active))
+      end
     end
   end
 
@@ -131,8 +213,30 @@ describe "EventPolicy" do
   end
 
   permissions :show? do
-    it 'allows normal users to view normal events' do
-      expect(subject).to permit(normal_user, Event.new)
+    context 'when event is imported' do
+      let(:organizer_user) { build_stubbed :user }
+      before do
+        allow(normal_user).to receive(:is_organizer?).and_return(false)
+        allow(staff_user).to receive(:is_organizer?).and_return(false)
+        allow(admin_user).to receive(:is_organizer?).and_return(false)
+        allow(organizer_user).to receive(:is_organizer?).and_return(true)
+      end
+
+      it 'does not allow normal users to view imported events' do
+        expect(subject).not_to permit(normal_user, Event.new(state: :imported))
+      end
+
+      it 'does not allow staff users to view imported events' do
+        expect(subject).not_to permit(staff_user, Event.new(state: :imported))
+      end
+
+      it 'allows admins to view imported events' do
+        expect(subject).to permit(admin_user, Event.new(state: :imported))
+      end
+
+      it 'allows organizers to view imported events' do
+        expect(subject).to permit(organizer_user, Event.new(state: :imported))
+      end
     end
 
     it 'does not allow normal users to view template events' do
@@ -140,8 +244,7 @@ describe "EventPolicy" do
     end
 
     it 'allows staff and admin users to view template events' do
-      expect(subject).to permit(staff_user, Event.new(template: true,
-        location: @location))
+      expect(subject).to permit(staff_user, Event.new(template: true, location: @location))
       expect(subject).to permit(admin_user, Event.new(template: true))
     end
   end
@@ -160,7 +263,7 @@ describe "EventPolicy" do
     let(:scope) { Event.all }
     subject(:policy_scope) { EventPolicy::Scope.new(user, scope).resolve }
 
-    permissions ".scope" do
+    permissions '.scope' do
       context 'for a normal user' do
         let(:user) { normal_user }
         it 'removes template events from event listing' do
