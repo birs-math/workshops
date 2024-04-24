cc5a04b182236d3d29acec37d9b3a8645002e282
Mikhail Matyukhin <itsmatyukhin@gmail.com>
2024-04-24 10:33:36 +0000

    feat: add puts instead of logger to debug

--- app/jobs/que/remind_expiring_invites_job.rb
+++ app/jobs/que/remind_expiring_invites_job.rb
@@ -3,7 +3,7 @@
 module Que
   class RemindExpiringInvitesJob < Job
     def run
-      Rails.logger.info "Running RemindExpiringInvitesJob at #{Time.current}"
+      puts "Running RemindExpiringInvitesJob at #{Time.current}"
       send_week_expiry_reminder
       send_day_expiry_reminder
     end
@@ -13,14 +13,14 @@ module Que
     def send_week_expiry_reminder
       Invitation.where(expires: (Date.current + 1.week).all_day).find_each do |invite|
         AttendanceConfirmationMailer.remind(invitation_id: invite.id, expiry_days: 7).deliver_now
-        Rails.logger.info "Sent reminder for #{invite.id} with expiry in 7 days"
+        puts "Sent reminder for #{invite.id} with expiry in 7 days"
       end
     end
 
     def send_day_expiry_reminder
       Invitation.where(expires: Date.current.all_day).find_each do |invite|
         AttendanceConfirmationMailer.remind(invitation_id: invite.id, expiry_days: 1).deliver_now
-        Rails.logger.info "Sent reminder for #{invite.id} with expiry today"
+        puts "Sent reminder for #{invite.id} with expiry today"
       end
     end
   end
