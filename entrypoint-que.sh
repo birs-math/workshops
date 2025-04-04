e8264b948705ec4adefb9caa875c569753134516
bigsnarfdude <ohprecio@gmail.com>
2025-04-04 07:36:42 -0600

    initial working version mac m2 silicon

--- entrypoint-que.sh
+++ entrypoint-que.sh
@@ -1,8 +1,28 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-source /etc/profile.d/rvm.sh
+# Wait for database to be ready
+until nc -z -v -w30 db 5432
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
 
-echo
-echo "Running que..."
-/usr/local/rvm/bin/rvm-exec 2.7.7 bundle exec que
+# Try connecting to the web service, but don't fail if it's not available yet
+echo "Trying to connect to web service..."
+MAX_ATTEMPTS=10
+ATTEMPT=0
+while [ $ATTEMPT -lt $MAX_ATTEMPTS ]; do
+  if nc -z -v -w2 web 8000; then
+    echo "Web service is up!"
+    break
+  fi
+  ATTEMPT=$((ATTEMPT+1))
+  echo "Web service not ready yet, attempt $ATTEMPT/$MAX_ATTEMPTS. Waiting..."
+  sleep 5
+done
+
+# Start the que worker
+echo "Starting Que worker..."
+exec bundle exec rake que:work
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,84 +1,31 @@
-#!/bin/bash
+#!/bin/sh
 set -e
 
-source /etc/profile.d/rvm.sh
+# Remove a potentially pre-existing server.pid for Rails.
+rm -f /home/app/workshops/tmp/pids/server.pid
 
-echo
-echo "Welcome to OS:"
-uname -v
-cat /etc/issue
-sed -i -e 's/mesg n .*true/tty -s \&\& mesg n/g' ~/.profile
+# Wait for database to be ready
+until nc -z -v -w30 db 5432
+do
+  echo "Waiting for database connection..."
+  # wait for 5 seconds before check again
+  sleep 5
+done
 
-
-echo
-echo "Setting system timezone..."
-export DEBIAN_FRONTEND=noninteractive DEBCONF_NONINTERACTIVE_SEEN=true
-echo "tzdata tzdata/Areas select America" > /tmp/tz.txt
-echo "tzdata tzdata/Zones/America select Edmonton" >> /tmp/tz.txt
-debconf-set-selections /tmp/tz.txt
-rm /etc/timezone
-rm /etc/localtime
-dpkg-reconfigure --frontend noninteractive tzdata
-
-echo
-echo "Ruby version:"
-ruby -v
-
-echo
-echo "Node version:"
-node --version
-
-echo
-echo "Yarn version:"
-yarn --version
-
-echo
-echo "Installing bundler..."
-/usr/local/rvm/bin/rvm-exec 2.7.7 gem install bundler -v 2.4.22
-
-echo
-echo "Bundle install..."
-su - app -c "cd /home/app/workshops; /usr/local/rvm/bin/rvm-exec 2.7.7 bundle install"
-
-if [ ! -d "${GEM_HOME}/gems" ]; then
-  echo
-  echo "Gems not found in $GEM_HOME!"
-  echo
-  exit
+# We're already running as the app user (from Dockerfile)
+# Run bundle if needed
+if [ -f Gemfile ] && [ ! -d vendor/bundle ]; then
+  bundle check || bundle install --jobs 4
 fi
 
-echo
-echo "Changing to non-root file permissions..."
-chown app:app -R /usr/local/rvm/gems
+# Skip Yarn - it's causing permission issues
+# if [ -f yarn.lock ]; then
+#   yarn install --check-files
+# fi
 
-echo
-echo "Running migrations..."
-/usr/local/rvm/bin/rvm-exec 2.7.7 bundle exec rails db:migrate
+# Run migrations if needed
+bundle exec rake db:migrate 2>/dev/null || bundle exec rake db:setup
 
-echo
-echo "Checking for WebPacker..."
-if [ ! -e /home/app/workshops/bin/webpack ]; then
-  echo "Installing webpacker..."
-  RAILS_ENV=production /usr/local/rvm/bin/rvm-exec 2.7.7 bundle exec rails webpacker:install
-  echo "Done!"
-  echo
-fi
-
-if [ ! -e /home/app/workshops/tmp ]; then
-  mkdir /home/app/workshops/tmp
-  mkdir -p /home/app/workshops/vendor/cache
-fi
-chown app:app -R /home/app/workshops
-
-echo
-echo "Compiling Assets..."
-su - app -c "cd /home/app/workshops; yarn install"
-su - app -c "cd /home/app/workshops; RAILS_ENV=production SECRET_KEY_BASE=token bundle exec rake assets:precompile --trace"
-su - app -c "cd /home/app/workshops; yarn"
-
-#echo
-#echo "Launching webpack-dev-server..."
-#su - app -c "ruby /home/app/workshops/bin/webpack-dev-server &"
-echo
-echo "Starting web server..."
-/usr/local/rvm/bin/rvm-exec 2.7.7 bundle exec passenger start #--min-instances 2
+# Start Rails server directly (don't try to use /sbin/my_init)
+echo "Starting Rails server on port 8000..."
+exec bundle exec rails server -b 0.0.0.0 -p 8000
